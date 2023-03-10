f17396368badb8007e130401e9444a581eb809fe
Skip Daly <59938360+encoura-skipdaly@users.noreply.github.com>
2023-03-10 18:29:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,13 +52,29 @@ streamlit.write('The user entered ', fruit_choice)
 # Don't run anything after this while we troubleshoot...
 streamlit.stop()
 
-# import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
+
+
 streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list();
+  with  my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
+        return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_data_rows = get_fruit_load_list()
 streamlit.dataframe(my_data_rows)
+
+#OLD CODE VERSION:
+# import snowflake.connector
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
+# my_data_rows = my_cur.fetchall()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add fruits?
 streamlit.header("What fruit would you like to add?")
