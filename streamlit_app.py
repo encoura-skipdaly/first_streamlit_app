5d888f2c6ebd741699c659f66365db139e05f458
Skip Daly <59938360+encoura-skipdaly@users.noreply.github.com>
2023-03-10 19:08:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ streamlit.write('The user entered ', fruit_choice)
 # Don't run anything after this while we troubleshoot...
 # streamlit.stop()
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!:")
 #Snowflake-related functions
 def get_fruit_load_list():
   with  my_cnx.cursor() as my_cur:
@@ -63,16 +63,18 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add fruits?
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        mycur.execute("INSERT into FRUIT_LOAD_LIST values ('" + {new_fruit} + "')")
+        mycur.execute("INSERT into FRUIT_LOAD_LIST values ('" + ???? + "')")
         return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
